25dea7f95827af5c1c458ec9d8e2e5eba4766133
engmbuthia01 <kevinmbuthia3354@gmail.com>
2024-08-27 20:36:03 -0700

    created a file /tmp

---
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -0,0 +1,9 @@
+# creating a file in /tmp using puppet
+
+file { '/tmp/school':
+  ensure  => 'file',
+  content => 'I love Puppet',
+  owner   => 'www-data',
+  group   => 'www-data',
+  mode    => '0744',
+}
